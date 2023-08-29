13737b12b479b0c1aa2cde610a497a99fde4930b
manel <manelgavalda1@gmail.com>
2023-08-29 11:04:22 +0200

    Update vite

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,8 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
+                'resources/css/app.css',
+                'resources/sass/app.scss',
                 'resources/js/app.js',
             ],
             refresh: true,
