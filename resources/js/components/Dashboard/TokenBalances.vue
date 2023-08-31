b239eeee195cba6f7e1f7310b280fcbafa5f70e2
manel <manelgavalda1@gmail.com>
2023-08-31 12:41:05 +0200

    Add dashboard image

--- resources/js/components/Dashboard/TokenBalances.vue
+++ resources/js/components/Dashboard/TokenBalances.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="col-span-full xl:col-span-4 bg-white dark:bg-slate-800 shadow-lg rounded-sm border border-slate-200 dark:border-slate-700">
         <header class="px-5 py-4 border-b border-slate-100 dark:border-slate-700">
-            <h2 class="font-semibold text-slate-800 dark:text-slate-100">{{ $filters.capitalized(network) }} {{ $filters.currencyUSD(total) }}</h2>
+            <h2 class="font-semibold text-slate-800 dark:text-slate-100">{{ $filters.capitalized(network) }} <div>{{ $filters.currencyUSD(total) }}</div></h2>
         </header>
         <div class="p-3">
 
