a9607938d06dd12bd375508b67087337d0d4bf62
manel <manelgavalda1@gmail.com>
2023-09-08 20:05:19 +0200

    Use pest syntax

--- tests/Feature/GetBalanceHistoryTest.php
+++ tests/Feature/GetBalanceHistoryTest.php
@@ -1,32 +1,23 @@
 <?php
 
-namespace Tests\Feature;
-
-use File;
-use Tests\TestCase;
 use App\Models\User;
 use App\Contracts\DatabaseService;
 
-class GetBalanceHistoryTest extends TestCase
-{
-     /** @test */
-    public function it_gets_the_balance_history()
-    {
-        $this->app->bind(DatabaseService::class, fn() =>
-            new FakeDatabaseService('fakeApiKey', 'fakeUrl')
-        );
+it('gets the balance history', function () {
+    $this->app->bind(DatabaseService::class, fn() =>
+        new FakeDatabaseService('fakeApiKey', 'fakeUrl')
+    );
 
-        $this->actingAs(User::factory()->create());
+    $this->actingAs(User::factory()->create());
 
-        $this->get('get-balance-history')
-             ->assertJsonCount(30)
-             ->assertSeeTextInOrder([
-                 '2023-09-06' => 60000,
-                 '2023-09-07' => 30000,
-                 '2023-09-08' => 10000,
-             ]);
-    }
-}
+    $this->get('get-balance-history')
+         ->assertJsonCount(30)
+         ->assertSeeTextInOrder([
+             '2023-09-06' => 60000,
+             '2023-09-07' => 30000,
+             '2023-09-08' => 10000,
+         ]);
+});
 
 class FakeDatabaseService implements DatabaseService
 {
