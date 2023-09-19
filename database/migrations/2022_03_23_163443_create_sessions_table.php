8e25f7d64e16ac1517499cb08d1f4ad510a3f6ef
manel <manelgavalda1@gmail.com>
2023-09-20 01:23:21 +0200

    remove timestamp from sessions table to avoid database update on login

--- database/migrations/2022_03_23_163443_create_sessions_table.php
+++ database/migrations/2022_03_23_163443_create_sessions_table.php
@@ -19,7 +19,6 @@ return new class extends Migration
             $table->string('ip_address', 45)->nullable();
             $table->text('user_agent')->nullable();
             $table->text('payload');
-            $table->integer('last_activity')->index();
         });
     }
 
