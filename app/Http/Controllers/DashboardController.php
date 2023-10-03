39fa8830e2fce90062426f30c03285702a53e946
manel <manelgavalda1@gmail.com>
2023-10-03 22:29:40 +0200

    Moving to functions

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -8,12 +8,25 @@ class DashboardController extends Controller
 {
     public function index(DatabaseService $databaseService)
     {
+        $tokens = $this->getTokens($databaseService);
+
+        $ethereumPrice = $tokens->firstWhere('pool', 'ETH (Mainnet)')->price;
+
+        $historicalBalances = $this->getHistoricalBalances($databaseService);
+
+        return view('pages/dashboard/dashboard', compact('tokens', 'ethereumPrice', 'historicalBalances'));
+    }
+
+    protected function getTokens($databaseService)
+    {
         $tokens = $databaseService->getTokens();
+
+        $rewards = $tokens->whereNotNull('parent');
 
         foreach($tokens as $token) {
             $token->rewards = [];
 
-            foreach($tokens->whereNotNull('parent') as $index => $reward) {
+            foreach($rewards as $index => $reward) {
                 if($reward->parent == $token->pool) {
                     unset($tokens[$index]);
 
@@ -22,14 +35,15 @@ class DashboardController extends Controller
             }
         }
 
-        $ethereumPrice = $tokens->firstWhere('pool', 'ETH (Mainnet)')->price;
+        return $tokens;
+    }
 
-        $historicalBalances = $databaseService->getHistoricalBalances()
+    protected function getHistoricalBalances($databaseService)
+    {
+        return $databaseService->getHistoricalBalances()
             ->mapWithKeys(fn ($item) => [ $item->created_at => [
                 'balance' => $item->balance,
                 'price' => $item->price
             ]]);
-
-        return view('pages/dashboard/dashboard', compact('tokens', 'ethereumPrice', 'historicalBalances'));
     }
 }
